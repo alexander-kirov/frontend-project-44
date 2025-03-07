8512d6a3e47d75953b14d084816bd427f428c5a8
Alexander Kirov <alexander.d.kirov@gmail.com>
2025-03-07 15:57:06 +0000

    add brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,42 @@
+import { getUserName, generateNumber, getAnswer } from '../index.js';
+
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  const name = getUserName();
+  console.log(`Hello, ${name}!`);
+
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  let i = 0;
+  while (i < 3) {
+    const num = generateNumber(1, 100);
+    console.log('ЧИСЛО', num);
+    let result = '';
+    const upperBorder = num / 2;
+    console.log(upperBorder);
+    for (let divisor = 2; divisor <= upperBorder; divisor += 1) {
+	    console.log('ДЕЛИТЕЛЬ', divisor)
+      const check = num % divisor;
+      if (check === 0) {
+        result = 'no';
+      } else {
+          result = 'yes';
+      }
+    }
+	  console.log('!!!!!!', result);
+    const question = `Question: ${num}`;
+    console.log(question);
+    const answer = getAnswer();
+
+    if (answer === result) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.
+Let's try again, ${name}`);
+      i = 4;
+      return i;
+    }
+  }
+  console.log(`Congratulations, ${name}`);
+};
